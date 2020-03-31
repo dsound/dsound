af9f8fdbddf8f96bb81e6ca0ab4f5a583c47afe2
prc <prcolaco@gmail.com>
2020-03-31 12:12:03 +0100

    publish version 0.4.3

---
+++ precache-manifest.c921cd5e6daafdb6e6370aabc1643c53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6068d1f9087cdc5982a34ee089c892ef",
+    "url": "/static/media/load.6068d1f9.gif"
+  },
+  {
+    "revision": "59c113149950791182851f7a06be210a",
+    "url": "/static/media/doe.59c11314.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "785c6175d1c7ddbf23bd",
+    "url": "/static/js/main.785c6175.chunk.js"
+  },
+  {
+    "revision": "c3b010af31d3d770658b",
+    "url": "/static/js/1.c3b010af.chunk.js"
+  },
+  {
+    "revision": "c3b010af31d3d770658b",
+    "url": "/static/css/1.5665d901.chunk.css"
+  },
+  {
+    "revision": "7d6bb57c8f16e4261d5c8a398283fb31",
+    "url": "/index.html"
+  }
+];
